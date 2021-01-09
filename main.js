6be5940395daea767623c40fc7596b3c80d82ade
rkjain119 <rkjain119@gmail.com>
2021-01-09 17:21:37 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+let inputTranslateText = document.querySelector("#input-text");
+let outputTranslateText = document.querySelector("#output-text");
+
+let btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", btnClick);
+
+let apiURL = "https://api.funtranslations.com/translate/shakespeare.json";
+
+
+function btnClick(e) {
+    let input = inputTranslateText.value;
+    let finalURL = constructURL(input);
+
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            outputTranslateText.innerText = json.contents.translated;
+        })
+        .catch(() => alert("Rate limiting on try later"))
+}
+
+function constructURL(inputText) {
+    let encodedURI = encodeURI(inputText);
+    return `${apiURL}? text=${encodedURI}`;
+}
